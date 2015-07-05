e710c4b6903e4d8cf410bc7aa9dbf5424ea4ee59
Brian Sermons <bsermons@gmail.com>
2015-07-04 22:43:14 -0500

    initial commit

---
+++ bs-elm/extensions.el
@@ -0,0 +1,36 @@
+;;; extensions.el --- bs-elm Layer extensions File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(setq bs-elm-pre-extensions
+      '(
+        ;; pre extension bs-elms go here
+        ))
+
+(setq bs-elm-post-extensions
+      '(
+        elm-mode
+        flycheck-elm
+        ))
+
+(defun bs-elm/init-elm-mode ()
+  "Initialize elm-mode"
+  )
+
+;; For each extension, define a function bs-elm/init-<extension-bs-elm>
+;;
+;; (defun bs-elm/init-my-extension ()
+;;   "Initialize my extension"
+;;   )
+;;
+;; Often the body of an initialize function uses `use-package'
+;; For more info on `use-package', see readme:
+;; https://github.com/jwiegley/use-package
