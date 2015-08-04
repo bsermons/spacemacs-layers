47d882674b27fa76078a3bf282b29b027ee96ec2
Brian Sermons <bsermons@gmail.com>
2015-08-04 05:56:59 -0500

    Fixing extensions

---
+++ bs-elm/config.el
@@ -0,0 +1,14 @@
+;;; config.el --- Elm Layer configuration File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Brian Sermons & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; Variables
+(spacemacs|defvar-company-backends elm-mode)
